c9fc68762c0e56c7cd013e65110bac96b3a46ef8
Partha Sarathi Dash <130224945+orewaparth@users.noreply.github.com>
2023-07-29 07:05:28 +0530

    yo

---
+++ task_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    /*to practice type casting*/
+    float n;
+   int b = 'a';
+   cout<< b << endl;
+   cout<<"enter a float";
+      cin>> n;
+   int c=n/3.0;
+   cout<< c << endl;
+   char ch ;
+   cout<<"enter a character:";
+   cin >> ch;
+   int z=ch;
+   cout<<z<<endl;
+cout<<'a'<<endl;
+
+
+   
+}
