de83dd4c2db0f7d63dd0fdaa5e6eded12d39b689
Partha Sarathi Dash <130224945+orewaparth@users.noreply.github.com>
2023-07-30 10:44:29 +0530

    you hemlo

--- task_3.cpp
+++ task_3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    /*to practice type casting*/
-    float n;
+    /*to practice type casting , cin,cin.get()*/
+   float n;
    int b = 'a';
    cout<< b << endl;
    cout<<"enter a float";
@@ -15,6 +15,11 @@ int main(){
    int z=ch;
    cout<<z<<endl;
 cout<<'a'<<endl;
+int x;
+cout<<"enter x";
+x=cin.get();
+cout<<x;
+
 
 
    
